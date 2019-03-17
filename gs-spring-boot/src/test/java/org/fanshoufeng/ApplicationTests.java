57ecdea947cfbeea77fe03c6ac45bc65334daa0e
fanshoufeng <shoufeng.fan@gmail.com>
2019-03-17 20:51:28 +0800

    修改测试类

--- gs-spring-boot/src/test/java/org/fanshoufeng/ApplicationTests.java
+++ gs-spring-boot/src/test/java/org/fanshoufeng/ApplicationTests.java
@@ -1,16 +1,32 @@
 package org.fanshoufeng;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	public void contextLoads() {
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(Matchers.equalTo("Greetings from Spring Boot!")));
 	}
 
 }
